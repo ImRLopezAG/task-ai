ea5dad775f5af0cdffbc096682b30ea3c3a3a64f
Angel Gabriel Lopez <angel178293lopez@gmail.com>
2024-01-13 22:38:19 -0400

    Refactor login page and add metadata retrieval

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,24 @@ import { App } from '@components/server/dashboard'
 import { getServerAuthSession } from '@server/auth'
 import { UserAuthForm } from '@client/login'
 import { getCsrfToken } from 'next-auth/react'
+import { getMetadata } from '@utils/metadata'
+
+export const metadata = getMetadata({ })
 
 export default async function Home (): Promise<JSX.Element> {
   const session = await getServerAuthSession()
-  const csrfToken = await getCsrfToken()
   return (
     <>
       {
         session
           ? (
             <App />
-
             )
           : (
-          <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-            <h1>Please log in</h1>
-            <UserAuthForm token={csrfToken ?? ''} />
-          </div>
+              <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+                <h1>Please log in</h1>
+                <UserAuthForm token={await getCsrfToken() ?? ''} />
+              </div>
             )
       }
     </>
