6b321ad106f2d484e45a91a1f6823b86fdbf90f6
Angel Gabriel Lopez TSX <88790737+ImRLopezAG@users.noreply.github.com>
2024-01-15 13:43:57 +0000

    Refactor TaskTypeSchema in schema.ts

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,6 +1,14 @@
 import { z } from 'zod'
 
-export const LabelSchema = z.enum(['bug', 'documentation', 'feature', 'development', 'lifting', 'internal', 'personal'])
+export const LabelSchema = z.enum([
+  'bug',
+  'documentation',
+  'feature',
+  'development',
+  'lifting',
+  'internal',
+  'personal'
+])
 export const PrioritySchema = z.enum(['high', 'low', 'medium'])
 
 export const StatusSchema = z.enum([
@@ -13,17 +21,33 @@ export const StatusSchema = z.enum([
   'personal'
 ])
 
-export const TaskTypeSchema = z.object({
-  id: z.string(),
-  title: z.string().min(3).max(50),
-  description: z.string(),
-  status: StatusSchema,
-  label: LabelSchema,
-  priority: PrioritySchema,
-  estimatedTime: z.string().transform((val) => val.replace(/[^0-9]/g, '')),
-  case: z.string().trim().toUpperCase(),
-  company: z.string().trim().toUpperCase(),
-  userId: z.string(),
-  createdAt: z.date().nullable().optional(),
-  updatedAt: z.date().nullable().optional()
-}).strict()
+export const TaskTypeSchema = z
+  .object({
+    id: z.string(),
+    title: z.string().min(3).max(50),
+    description: z.string(),
+    status: StatusSchema,
+    label: LabelSchema,
+    priority: PrioritySchema,
+    estimatedTime: z.coerce.string(),
+    case: z.coerce
+      .string()
+      .trim()
+      .toUpperCase()
+      .transform((val) => {
+        if (val === '') return 'NONE'
+        else return val
+      }),
+    company: z.coerce
+      .string()
+      .trim()
+      .toUpperCase()
+      .transform((val) => {
+        if (val === '') return 'NONE'
+        else return val
+      }),
+    userId: z.string(),
+    createdAt: z.date().nullable().optional(),
+    updatedAt: z.date().nullable().optional()
+  })
+  .strict()
